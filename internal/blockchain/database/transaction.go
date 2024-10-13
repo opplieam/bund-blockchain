da9b94838b29d0d89b722cd522a95f47f100a479
Opp Lieam <runicer156@gmail.com>
2024-10-14 04:40:21 +0700

    feat(database): add blockchain database

---
+++ internal/blockchain/database/transaction.go
@@ -0,0 +1,49 @@
+package database
+
+import (
+	"crypto/ecdsa"
+	"math/big"
+
+	"github.com/opplieam/bund-blockchain/internal/utils/signature"
+)
+
+// Tx is the transactional information between two parties.
+type Tx struct {
+	ChainID uint16    `json:"chain_id"` // Ethereum: The chain id that is listed in the genesis file.
+	Nonce   uint64    `json:"nonce"`    // Ethereum: Unique id for the transaction supplied by the user.
+	FromID  AccountID `json:"from"`     // Ethereum: Account sending the transaction. Will be checked against signature.
+	ToID    AccountID `json:"to"`       // Ethereum: Account receiving the benefit of the transaction.
+	Value   uint64    `json:"value"`    // Ethereum: Monetary value received from this transaction.
+	Tip     uint64    `json:"tip"`      // Ethereum: Tip offered by the sender as an incentive to mine this transaction.
+	Data    []byte    `json:"data"`     // Ethereum: Extra data related to the transaction.
+}
+
+// SignedTx is a signed version of the transaction. This is how clients like
+// a wallet provide transactions for inclusion into the blockchain.
+type SignedTx struct {
+	Tx
+	V *big.Int `json:"v"` // Ethereum: Recovery identifier, either 29 or 30 with bundID.
+	R *big.Int `json:"r"` // Ethereum: First coordinate of the ECDSA signature.
+	S *big.Int `json:"s"` // Ethereum: Second coordinate of the ECDSA signature.
+}
+
+// Sign uses the specified private key to sign the transaction.
+func (tx Tx) Sign(privateKey *ecdsa.PrivateKey) (SignedTx, error) {
+
+	// Sign the transaction with the private key to produce a signature.
+	v, r, s, err := signature.Sign(tx, privateKey)
+	if err != nil {
+		return SignedTx{}, err
+	}
+
+	// Construct the signed transaction by adding the signature
+	// in the [R|S|V] format.
+	signedTx := SignedTx{
+		Tx: tx,
+		V:  v,
+		R:  r,
+		S:  s,
+	}
+
+	return signedTx, nil
+}
---
+++ internal/utils/signature/signature.go
@@ -0,0 +1,67 @@
+package signature
+
+import (
+	"crypto/ecdsa"
+	"encoding/json"
+	"fmt"
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/crypto"
+)
+
+// bundID is an arbitrary number for signing messages. This will make it
+// clear that the signature comes from the Bund blockchain.
+// Ethereum and Bitcoin do this as well, but they use the value of 27.
+const bundID = 29
+
+// Sign uses the specified private key to sign the data.
+func Sign(value any, privateKey *ecdsa.PrivateKey) (v, r, s *big.Int, err error) {
+
+	// Prepare the data for signing.
+	data, err := stamp(value)
+	if err != nil {
+		return nil, nil, nil, err
+	}
+
+	// Sign the hash with the private key to produce a signature.
+	sig, err := crypto.Sign(data, privateKey)
+	if err != nil {
+		return nil, nil, nil, err
+	}
+
+	// Convert the 65 byte signature into the [R|S|V] format.
+	v, r, s = toSignatureValues(sig)
+
+	return v, r, s, nil
+
+}
+
+// stamp returns a hash of 32 bytes that represents this data with
+// the Bund stamp embedded into the final hash.
+func stamp(value any) ([]byte, error) {
+
+	// Marshal the data.
+	v, err := json.Marshal(value)
+	if err != nil {
+		return nil, err
+	}
+
+	// This stamp is used so signatures we produce when signing data
+	// are always unique to the Bund blockchain.
+	stamp := []byte(fmt.Sprintf("\x19Bund Signed Message:\n%d", len(v)))
+
+	// Hash the stamp and txHash together in a final 32 byte array
+	// that represents the data.
+	data := crypto.Keccak256(stamp, v)
+
+	return data, nil
+}
+
+// toSignatureValues converts the signature into the r, s, v values.
+func toSignatureValues(sig []byte) (v, r, s *big.Int) {
+	r = big.NewInt(0).SetBytes(sig[:32])
+	s = big.NewInt(0).SetBytes(sig[32:64])
+	v = big.NewInt(0).SetBytes([]byte{sig[64] + bundID})
+
+	return v, r, s
+}
